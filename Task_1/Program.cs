c5f01b9a29ff9fab692bc4bfd5b46d1848901b97
Liudmila <to_shagalova_lu@mail.ru>
2022-09-16 18:07:59 +0500

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int numberA = -9;
+int numberB = 7;
+
+if (numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberA);
+    Console.Write("min = ");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+    Console.Write("min = ");
+    Console.WriteLine(numberA);
+}
